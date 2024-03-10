d4d3d4ed693bd43a9c409ac4920c1cfe5776efdb
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-03-10 15:21:26 +0530

    Changed the text color of active link via isActive prop of NavLink

--- react-router-basics/src/components/Navbar.jsx
+++ react-router-basics/src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+
+  const navLinkStyles = (isActive) => {
+    
+  }
+
   return (
     <div className='flex justify-center bg-green-600 p-4'>
         <nav className='flex gap-3'>
-            <Link to="/">Home</Link>
-            <Link to="about">About</Link>
+            {/* <Link to="/">Home</Link>
+            <Link to="about">About</Link> */}
+
+            <NavLink to="/" className={({isActive}) => `${isActive ? 'text-orange-700' : 'text-gray-700'} `}>Home</NavLink>
+            <NavLink to="/about" className={({isActive}) => `${isActive ? 'text-orange-700' : 'text-gray-700'} `}>About</NavLink>
+            
         </nav>
     </div>
   )
