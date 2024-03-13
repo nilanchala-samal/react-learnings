f2705ae3cd509908a3f28878a5ade750534d7ee7
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-03-14 02:51:19 +0530

    added one more action and used bindActionCreators

--- REDUX-DEMO/index.js
+++ REDUX-DEMO/index.js
@@ -1,19 +1,34 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
 
+
+// 1.
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
+
+// 2.
 function orderCake() {    // This is the 'action creator' function which returns an object
     return {    // The returned object is the action
         type: CAKE_ORDERED,
-        quantity: 1
+        payload: 1
     }
 }
 
+function restockCake(qty = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty
+    }
+}
+
+// 3.
 const initialState = {  // initial state
     numberOfCakes: 10
 }
 
+// 4.
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case CAKE_ORDERED:
@@ -21,11 +36,17 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numberOfCakes: state.numberOfCakes - 1
             }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numberOfCakes: state.numberOfCakes + action.payload
+            }
         default:
             return state
     }
 }
 
+// 5.
 const store = createStore(reducer)
 console.log('Initial State: ', store.getState())
 
@@ -33,11 +54,20 @@ const unsubscribe = store.subscribe(() =>
     console.log('Updated state', store.getState())
 )
 
-store.dispatch(orderCake())   // dispatch method accepts the action
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+// store.dispatch(orderCake())   // dispatch method accepts the action
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+
+// store.dispatch(restockCake(3))
+// store.dispatch(restockCake())
+
+const actions = bindActionCreators({orderCake, restockCake}, store.dispatch)
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
+actions.restockCake()
 
 unsubscribe()  // once unsubscribed, the dispatch method will not cause the subscribe() to run
 
 store.dispatch(orderCake())
-console.log('Nothing happened above')
