ee71ef04a0ff1c3100561efa2be73b01da228f55
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-03-19 01:47:46 +0530

    dispatch actions using useDispatch() hook

--- react-reduxtoolkit/src/features/cake/CakeView.jsx
+++ react-reduxtoolkit/src/features/cake/CakeView.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux' // import the useDispatch() hook
+import { ordered, restocked } from './cakeSlice'  // import the action creators
 
 const CakeView = () => {
+  const [value, setValue] = useState(1);
   const numOfCakes = useSelector((state) => state.cake.numberOfCakes)
+  const dispatch = useDispatch()   // call useDispatch() hook 
   return (
     <div>
       <h2>Number of Cakes: {numOfCakes}</h2>
-      <button>Order Cake</button>
-      <button>Restock Cakes</button>
+      <button onClick={() => dispatch(ordered())}>Order Cake</button><br />
+      <input type="number" value={value} onChange={(e) => setValue(parseInt(e.target.value))}/><br/>
+      <button onClick={() => dispatch(restocked(value))}>Restock Cakes</button>
     </div>
   )
 }
 
 export default CakeView
+
--- react-reduxtoolkit/src/features/icecream/IceCreamView.jsx
+++ react-reduxtoolkit/src/features/icecream/IceCreamView.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './iceCreamSlice'
 
 const IceCreamView = () => {
   const numOfIceCreams = useSelector((store) => store.iceCream.nubmerOfIceCreams)
+  const dispatch = useDispatch()
+
+  const handleOrderIcecream = () => {
+    return dispatch(ordered())
+  }
+  const handleRestockIcecreams = () => {
+    return dispatch(restocked(10))
+  }
   return (
     <div>
       <h2>Number of Icecream: {numOfIceCreams}</h2>
-      <button onClick={handlOrderIcecream}>Order Icecream</button>
+      <button onClick={handleOrderIcecream}>Order Icecream</button>
       <button onClick={handleRestockIcecreams}>Resotck Icecream</button>
     </div>
   )
