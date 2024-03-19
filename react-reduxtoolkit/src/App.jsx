ae6341dd505a3c32425239fa5f9a08ffcde1fabe
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-03-19 13:56:34 +0530

    completed the userView component

--- react-reduxtoolkit/src/App.jsx
+++ react-reduxtoolkit/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <CakeView />
       <IceCreamView />
-      {/* <UserView /> */}
+      <UserView />
     </>
   )
 }
--- react-reduxtoolkit/src/features/users/UserView.jsx
+++ react-reduxtoolkit/src/features/users/UserView.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from './userSlice';
 
-const UserView = () => {
+function UserView() {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div>
       <h2>List of Users: </h2>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {
+        !user.loading && user.users.length > 0 ? (
+          <ul>
+            {
+              user.users.map(user => (<li key={user.id}>{user.name}</li>))
+            }
+          </ul>
+        ) : null
+      }
     </div>
-  )
+  );
 }
 
-export default UserView
+export default UserView;
+
+
+
